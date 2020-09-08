cde4a44defdda07c77c8ea9cf17299452eb7ea28
nzhul <dobromirivanov1@gmail.com>
2020-09-08 23:56:28 +0300

    add some styling to the reactivities app

--- Reactivities/client-app/src/app/stores/activityStore.ts
+++ Reactivities/client-app/src/app/stores/activityStore.ts
@@ -3,7 +3,7 @@ import { createContext, SyntheticEvent } from 'react';
 import { IActivity } from '../models/activity';
 import agent from '../api/agent';
 
-configure({enforceActions: 'always'});
+configure({ enforceActions: 'always' });
 
 class ActivityStore {
   @observable activityRegistry = new Map();
@@ -13,9 +13,21 @@ class ActivityStore {
   @observable target = '';
 
   @computed get activitiesByDate() {
-    return Array.from(this.activityRegistry.values()).sort(
+    return this.groupActivitiesByDate(Array.from(this.activityRegistry.values()));
+  }
+
+
+
+  groupActivitiesByDate(activities: IActivity[]) {
+    const sortedActivities = activities.sort(
       (a, b) => Date.parse(a.date) - Date.parse(b.date)
     );
+
+    return Object.entries(sortedActivities.reduce((activities, activity) => {
+      const date = activity.date.split('T')[0];
+      activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+      return activities;
+    }, {} as { [key: string]: IActivity[] }));
   }
 
   // see: we use runInAction because of mobx strict mode!
@@ -33,8 +45,9 @@ class ActivityStore {
           this.activityRegistry.set(activity.id, activity);
         });
         this.loadingInitial = false;
-      })
+      });
 
+      console.log(this.groupActivitiesByDate(activities))
     } catch (error) {
       runInAction('load activities error', () => {
         this.loadingInitial = false;
@@ -50,7 +63,7 @@ class ActivityStore {
       this.loadingInitial = true;
       try {
         activity = await agent.Activities.details(id);
-        runInAction('getting activity',() => {
+        runInAction('getting activity', () => {
           this.activity = activity;
           this.loadingInitial = false;
         })
