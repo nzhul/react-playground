74ec9ffbcf333264a1e675fbb6c98730920c7a97
nzhul <dobromirivanov1@gmail.com>
2020-10-09 12:22:36 +0300

    adding follow functionality for both client and api

--- Reactivities/Application/Activities/MappingProfile.cs
+++ Reactivities/Application/Activities/MappingProfile.cs
@@ -12,7 +12,8 @@ namespace Application.Activities
       CreateMap<UserActivity, AttendeeDto>()
       .ForMember(d => d.Username, opt => opt.MapFrom(s => s.AppUser.UserName))
       .ForMember(d => d.DisplayName, opt => opt.MapFrom(s => s.AppUser.DisplayName))
-      .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
+      .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url))
+      .ForMember(d => d.Following, o => o.MapFrom<FollowingResolver>());
     }
   }
 }
--- Reactivities/Application/Profiles/Profile.cs
+++ Reactivities/Application/Profiles/Profile.cs
@@ -1,18 +1,26 @@
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 using Domain;
 
 namespace Application.Profiles
 {
-    public class Profile
-    {
-        public string DisplayName { get; set; }
+  public class Profile
+  {
+    public string DisplayName { get; set; }
 
-        public string Username { get; set; }
+    public string Username { get; set; }
 
-        public string Image { get; set; }
+    public string Image { get; set; }
 
-        public string Bio { get; set; }
+    public string Bio { get; set; }
 
-        public ICollection<Photo> Photos { get; set; }
-    }
+    [JsonPropertyName("following")]
+    public bool IsFollowed { get; set; }
+
+    public int FollowersCount { get; set; }
+
+    public int FollowingCount { get; set; }
+
+    public ICollection<Photo> Photos { get; set; }
+  }
 }
